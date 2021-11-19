f13a5bcc8756b90d4ce725f5c035d24866620fe0
朱正清 <zhengqing.zhu@tcl.com>
2021-11-19 16:32:38 +0800

    1

---
+++ src/main/java/203.移除链表元素.java
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=java
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    ListNode(int[] arr) {
+        if (arr == null || arr.length == 0)
+            throw new IllegalArgumentException("Wrong arr");
+        this.val = arr[0];
+
+        ListNode cur = this;
+        for (int i = 1; i < arr.length; i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String connector = " -> ";
+        StringBuilder res = new StringBuilder();
+        for (ListNode cur = this; cur != null; cur = cur.next)
+            res.append(cur.val + connector);
+        res.append("NULL");
+        return res.toString();
+    }
+}
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null)
+            return head;
+
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
+}
+// @lc code=end
+
