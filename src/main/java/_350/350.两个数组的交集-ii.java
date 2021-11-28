f4820f14b0244fb3352c4b867a55135fabb5f917
朱正清 <zhengqing.zhu@tcl.com>
2021-11-29 01:00:33 +0800

    1

---
+++ src/main/java/_350/350.两个数组的交集-ii.java
@@ -0,0 +1,40 @@
+package _350;/*
+ * @lc app=leetcode.cn id=350 lang=java
+ *
+ * [350] 两个数组的交集 II
+ */
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+// @lc code=start
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums1) {
+            if (map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else
+                map.put(num, 1);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                list.add(num);
+                if (map.get(num) == 1)
+                    map.remove(num);
+                else
+                    map.put(num, map.get(num) - 1);
+            }
+        }
+
+        int[] res = new int[list.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
