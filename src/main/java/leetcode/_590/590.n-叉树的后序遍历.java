1bff469866038396b72944ef2392e38da2191c46
朱正清 <zhengqing.zhu@tcl.com>
2022-03-13 19:04:15 +0800

    1

--- src/main/java/leetcode/_590/590.n-叉树的后序遍历.java
+++ src/main/java/leetcode/_590/590.n-叉树的后序遍历.java
@@ -24,11 +24,52 @@ class Node {
 };
 */
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
+
     public List<Integer> postorder(Node root) {
-        return null;
+//        return recursive(root);
+        return nonRecursive(root);
+    }
+
+    public List<Integer> nonRecursive(Node root) {
+        List<Integer> vals = new LinkedList<>();
+        Deque<Object[]> stack = new LinkedList<>();
+        stack.push(new Object[]{0, root});
+        while (!stack.isEmpty()) {
+            Object[] poped = stack.pop();
+            Integer num = (Integer) poped[0];
+            Node cur = (Node) poped[1];
+
+            if (cur == null)
+                continue;
+            if (num == cur.children.size())
+                vals.add(cur.val);
+
+            if (num < cur.children.size()) {
+                stack.push(new Object[]{num + 1, cur});
+                stack.push(new Object[]{0, cur.children.get(num)});
+            }
+        }
+        return vals;
+    }
+
+    private List<Integer> recursive(Node root) {
+        List<Integer> vals = new LinkedList<>();
+        postorder(root, vals);
+        return vals;
+    }
+
+    private void postorder(Node node, List<Integer> vals) {
+        if (node == null)
+            return;
+
+        List<Node> children = node.children;
+        children.forEach(c -> postorder(c, vals));
+        vals.add(node.val);
     }
 }
 // @lc code=end
