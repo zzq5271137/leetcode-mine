5ac849b72df700c14372cee723e0bceffa150ee3
朱正清 <zhengqing.zhu@tcl.com>
2022-03-18 23:03:33 +0800

    1

--- src/main/java/leetcode/_160/160.相交链表.java
+++ src/main/java/leetcode/_160/160.相交链表.java
@@ -23,11 +23,57 @@ import java.util.Set;
 class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 //        return useHash(headA, headB);
-        return useDoublePointer(headA, headB);
+//        return useDoublePointer(headA, headB);
+        return useDoublePointer2(headA, headB);
+    }
+
+    private ListNode useDoublePointer2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode cur1 = headA;
+        ListNode cur2 = headB;
+
+        /*
+         * 计算出两个链表的长度差
+         */
+        int n = 0;
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+
+        if (cur1 != cur2)
+            return null;
+
+        cur1 = n > 0 ? headA : headB;  // 谁长，谁的头变成cur1
+        cur2 = cur1 == headA ? headB : headA;  // 谁短，谁的头变成cur2
+
+        n = Math.abs(n);
+
+        /*
+         * 先消耗掉长度相差的部分
+         */
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
     }
 
     private ListNode useDoublePointer(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null) return null;
+        if (headA == null || headB == null)
+            return null;
         ListNode pA = headA, pB = headB;
         while (pA != pB) {
             pA = pA == null ? headB : pA.next;
