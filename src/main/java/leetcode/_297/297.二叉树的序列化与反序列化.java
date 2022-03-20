d50d7314156b24890dc406f9943fdc93be1f9ffa
朱正清 <zhengqing.zhu@tcl.com>
2022-03-20 19:57:51 +0800

    1

--- src/main/java/leetcode/_297/297.二叉树的序列化与反序列化.java
+++ src/main/java/leetcode/_297/297.二叉树的序列化与反序列化.java
@@ -7,6 +7,7 @@ package leetcode._297;/*
 // @lc code=start
 
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -21,8 +22,32 @@ import java.util.List;
  */
 class Codec {
 
+    /*
+     * 序列化
+     */
     public String serialize(TreeNode root) {
-        return preOrderSerialize(root, "");
+//        return preOrderSerialize(root, "");
+        return levelOrderSerialize(root);
+    }
+
+    private String levelOrderSerialize(TreeNode root) {
+        if (root == null)
+            return "null,";
+
+        StringBuilder res = new StringBuilder();
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            if (node == null) {
+                res.append("null,");
+            } else {
+                res.append(node.val).append(",");
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        return res.toString();
     }
 
     private String preOrderSerialize(TreeNode node, String res) {
@@ -37,10 +62,40 @@ class Codec {
         return res;
     }
 
-
+    /*
+     * 反序列化
+     */
     public TreeNode deserialize(String data) {
         List<String> dataList = new LinkedList<>(Arrays.asList(data.split(",")));
-        return preOrderDeserialize(dataList);
+//        return preOrderDeserialize(dataList);
+        return levelOrderDeserialize(dataList);
+    }
+
+    private TreeNode levelOrderDeserialize(List<String> dataList) {
+        String val = dataList.remove(0);
+        TreeNode head = null;
+        if (!val.equals("null")) {
+            Deque<TreeNode> queue = new LinkedList<>();
+            head = new TreeNode(Integer.parseInt(val));
+            queue.add(head);
+            TreeNode node;
+            while (!queue.isEmpty()) {
+                node = queue.remove();
+                node.left = generateNode(dataList.remove(0));
+                node.right = generateNode(dataList.remove(0));
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+        }
+        return head;
+    }
+
+    private TreeNode generateNode(String val) {
+        if (val.equals("null"))
+            return null;
+        return new TreeNode(Integer.parseInt(val));
     }
 
     private TreeNode preOrderDeserialize(List<String> dataList) {
