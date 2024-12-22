98e23c3521456e3235e1cb0e0731967bf417565f
shuitun <shuitun@pinduoduo.com>
2024-12-22 20:51:56 +0800

    dp

--- src/main/java/leetcode/_122/122.买卖股票的最佳时机-ii.java
+++ src/main/java/leetcode/_122/122.买卖股票的最佳时机-ii.java
@@ -10,7 +10,9 @@ class Solution {
     private int res;
 
     public int maxProfit(int[] prices) {
-        return this.bruteForce(prices);
+//        return this.bruteForce(prices);
+//        return this.greedy(prices);
+        return this.dp(prices);
     }
 
     //超时
@@ -48,6 +50,46 @@ class Solution {
         }
     }
 
+    //贪心
+    public int greedy(int[] prices) {
+        int totalProfit = 0;
+        // 遍历价格数组，只要后一天比前一天价格高，就可以进行交易
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                totalProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return totalProfit;
+    }
+
+    //动态规划
+    public int dp(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        // 状态定义
+        // dp[i][0] 表示第i天不持有股票的最大利润
+        // dp[i][1] 表示第i天持有股票的最大利润
+        int[][] dp = new int[len][2];
+
+        // 初始状态
+        dp[0][0] = 0;                // 第一天不持有
+        dp[0][1] = -prices[0];       // 第一天买入
+
+        // 状态转移
+        for (int i = 1; i < len; i++) {
+            // 第i天不持有股票的最大利润 = max(前一天不持有，前一天持有今天卖出)
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+
+            // 第i天持有股票的最大利润 = max(前一天持有，前一天不持有今天买入)
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+
+        // 返回结果
+        return Math.max(dp[len - 1][0], dp[len - 1][1]);
+    }
 }
 // @lc code=end
 
